2fef793327893bb03bdb1e885707642feab4f9e5
wsw8186 <wsw8186@naver.com>
2024-09-10 17:48:06 +0900

    0910

--- src/C01/C01SystemOut.java
+++ src/C01/C01SystemOut.java
@@ -20,6 +20,12 @@ public class C01SystemOut {
 		
 		
 		//서식문자
+		//System.out.printf()
+		//format : 형식,서식
+		//%d : 10진수 정수 서식문자
+		//%f : 10진수 실수 서식문자
+		//%c : 한문자 서식문자
+		//%s : 문자열 서식문자
 //		System.out.printf("%d %d %d \n", 10,20,30);
 //		System.out.printf("%f %f %f \n", 10.1,20.1,30.1);
 //		System.out.printf("%c %c %c \n", 'A','B','C');
