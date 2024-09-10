9de16269c56cd1568e525791b721afe74940494d
wsw8186 <wsw8186@naver.com>
2024-09-10 17:41:35 +0900

    0910

--- src/C00/C00Helloworld.java
+++ src/C00/C00Helloworld.java
@@ -1,9 +1,16 @@
-package C00;
+package C00; //패키지명
 
-public class C00Helloworld {
-
-	public static void main(String[] args) {
-		System.out.println("HELLO WORLD");
+public class C00Helloworld //클래스영역 - 객체지향 문법 적용단위
+{
+	//메서드(함수) : 작업수행을 하는데 필요한 여러 코드를 묶어서 실행하는단위
+	//main 메서드 :  최초 실행 함수
+	//library 메서드 : 미리 만들어져 제공되어지는 메서드
+	//사용자 정의 메서드 : 개발자에 의해 만들어지는 메서드
+	
+	
+	public static void main(String[] args) //메서드 영억 -절차지향 문법 적용 단위
+	{
+		System.out.println("HELLO WORLD");//라이브러리 메서드 호출
 		
 	}
 
---
+++ src/C01/C01SystemOut.java
@@ -0,0 +1,38 @@
+package C01;
+
+public class C01SystemOut {
+
+	public static void main(String[] args) {
+		
+		//System : OS
+		//System.out : OS가 관리하는 표준 출력 스트림
+
+		//System.out.print()
+		//ESCAPE문자 :
+		// \n : 개행, 줄바꿈
+		// \b : 백스페이스
+		// \t : 탭길이(기본 8칸) 만큼 커서이동
+		
+//		System.out.print("HELLO WORLD\n");
+//		System.out.print("HELLO WORLD\n");
+//		System.out.print("HELLO WORLD\n");
+//		System.out.print("HELLO\t\t WORLD\n");
+		
+		
+		//서식문자
+//		System.out.printf("%d %d %d \n", 10,20,30);
+//		System.out.printf("%f %f %f \n", 10.1,20.1,30.1);
+//		System.out.printf("%c %c %c \n", 'A','B','C');
+//		System.out.printf("%s %s %s \n", "This is","String","Test");
+//		System.out.printf("%d.%s : %d\n", 1,"국어",100);
+
+		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD");
+		System.out.println("HELLO WORLD");
+		
+		
+		System.out.println("I SAY \"HELLO\"");
+			
+	}
+
+}
